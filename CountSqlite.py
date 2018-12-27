5a84731794d8fdce27f2299513f7f0d407ff91b5
yuu-eguci <yuu.eguci@gmail.com>
2018-12-27 18:01:19 +0900

    Add CountSqlite.py

---
+++ CountSqlite.py
@@ -0,0 +1,43 @@
+
+"""Record Counter for Sqlite
+
+""" 
+
+import sqlite3
+from contextlib import closing
+from pprint import pprint
+
+
+def count_tables(dbname):
+
+    # 接続します。
+    with closing(sqlite3.connect('test.sqlite3')) as con:
+
+        # おやくそく。
+        con.row_factory = sqlite3.Row
+
+        # テーブル一覧取得します。
+        sql = ' '.join([
+            'SELECT',
+                'tbl_name',
+            'FROM sqlite_master',
+            'WHERE type=:type',
+            'ORDER BY tbl_name',
+        ])
+        bind = { 'type': 'table', }
+        dic = {dict(row)['tbl_name']:0 for row in con.execute(sql, bind)}
+
+        # 各テーブルに count をかけます。
+        for table_name in dic:
+            sql = ' '.join([
+                'SELECT',
+                    'COUNT(0) count',
+                f'FROM {table_name}',
+            ])
+            dic[table_name] = con.execute(sql).fetchone()['count']
+
+    return dic
+
+
+if __name__ == '__main__':
+    pprint(count_tables('test.sqlite3'))
